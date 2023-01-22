39d138795795d4439a6b8b410fd7abdf4139dd01
Ashiq <ashiqul7362@gmail.com>
2023-01-22 17:13:21 +0500

    Card added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const somedata = document.getElementById("something");
+const card = document.getElementsByClassName("card")[0];
+let color = card.style.color
+
+card.addEventListener('click',()=>{
+    color = "blue"
+    card.style.backgroundColor = `${color}`;
+})
